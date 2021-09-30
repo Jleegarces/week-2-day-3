559d63e8e226899bee15dad41879bc7b819c2a1b
Jleegarces <86203137+Jleegarces@users.noreply.github.com>
2021-09-30 02:46:41 -0400

    Add files via upload

---
+++ jonsmod.py
@@ -0,0 +1,10 @@
+def houseArea(width, length):
+    area = width * length 
+    return area 
+
+living_room = {'houseArea': 1, 2}
+master_room = {'houseArea': 2, 4}
+
+totalArea = {'living_room' + 'master_room'}
+
+print(f"this house has an area of" + ('totalArea') + " square meters ")
